39c839cea3360a824e4ce97872d9e3622b0a1552
surajsingh34 <119447712+surajsingh34@users.noreply.github.com>
2022-12-02 21:49:06 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,32 @@
-let number = 13;
+//Problem 1 : Check whether a number is Prime or not?
 
-//check palindrome
+
+let number = 13;
+for(let i=1;i<=13;i++){
+if(number[i]%2===0){
+ count++; 
+}
+}
+
+if(count===2){
+  console.log("it is prime number")
+}
+else{
+  console.log("it is not prime number")
+}
+
+
+
+//Problem 2 : Check whether a string is palindrome or not.
+
+let string="apple"
+let flag=false;
+for(let i=string.length-1;i<=0;i--){
+ flag=true; 
+}
+if (flag===true){
+  console.log("yes string is palindrome")
+}
+else{
+  console.log("No string is palindrome")
+}
